954ef4b839f70e3cf9ab977e106e756c4bffe6cc
Laila Danguir <laila.danguir@gmail.com>
2024-04-03 10:10:33 +0000

    Endpoint Doctors

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const doctorRoutes = require('./src/Routes/doctorRoute');
 
+
+const app = express();
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT;
 const uri = process.env.MONGODB_URI;
 
-const app = express();
+
+
+app.use('/doctors', doctorRoutes);
+
 
 //connection a la base de donne
 mongoose
@@ -17,8 +25,6 @@ mongoose
     .catch((error) => {
         console.log('Error connecting to database: ', error)
     });
-
-
 
 app.listen(port, () => {
     console.log(`listening to port ${port}`)
