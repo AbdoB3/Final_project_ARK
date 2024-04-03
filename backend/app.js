3a979cbc3edf126b3177c3deb4fc28677ba97b74
maryam labyed <maryamlabyed4@gmail.com>
2024-04-03 12:16:10 +0000

    commit with src

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const consultationRoutes = require('./Routes/ consultationRoutes');
+const consultationRoutes = require('./src/Routes/ consultationRoutes');
 
 const app = express();
 
