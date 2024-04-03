84036717010d4b00327190b400e9df3486a9b1ca
AbdoB3 <R139591066@taalim.ma>
2024-04-03 10:29:01 +0000

    patient endpoints

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,11 @@ const port = process.env.PORT;
 const uri = process.env.MONGODB_URI;
 
 const app = express();
+app.use(express.json());
 
 //connection a la base de donne
 mongoose
-    .connect(uri)
+.connect(uri)
 
     .then(() => {
         console.log('Connected to database');
@@ -18,7 +19,10 @@ mongoose
         console.log('Error connecting to database: ', error)
     });
 
-
+const postPatient = require('./routes/patientRoutes')
+app.use('/patient',postPatient)
+    
+    
 
 app.listen(port, () => {
     console.log(`listening to port ${port}`)
